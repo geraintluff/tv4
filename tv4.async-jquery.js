0bef0ded94141de6897775f4cc781259d9fc3cd8
Peter <frauenglass@everbluedesign.com>
2013-07-07 17:47:55 -0700

    Added support for checkRecursive setting

--- tv4.async-jquery.js
+++ tv4.async-jquery.js
@@ -3,16 +3,16 @@
 //     callback(result, error);
 if (typeof (tv4.asyncValidate) === 'undefined') {
 	tv4.syncValidate = tv4.validate;
-	tv4.validate = function (data, schema, callback) {
+	tv4.validate = function (data, schema, callback, checkRecursive) {
 		if (typeof (callback) === 'undefined') {
-			return this.syncValidate(data, schema);
+			return this.syncValidate(data, schema, checkRecursive);
 		} else {
-			return this.asyncValidate(data, schema, callback);
+			return this.asyncValidate(data, schema, callback, checkRecursive);
 		}
 	};
-	tv4.asyncValidate = function (data, schema, callback) {
+	tv4.asyncValidate = function (data, schema, callback, checkRecursive) {
 		var $ = jQuery;
-		var result = tv4.validate(data, schema);
+		var result = tv4.validate(data, schema, checkRecursive);
 		if (!tv4.missing.length) {
 			callback(result, tv4.error);
 		} else {
@@ -27,7 +27,7 @@ if (typeof (tv4.asyncValidate) === 'undefined') {
 			});
 			// When all requests done, try again
 			$.when.apply($, missingSchemas).done(function () {
-				var result = tv4.asyncValidate(data, schema, callback);
+				var result = tv4.asyncValidate(data, schema, callback, checkRecursive);
 			});
 		}
 	};
