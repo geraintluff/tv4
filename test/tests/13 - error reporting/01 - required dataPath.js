61096b18713d54df158c4d7b01f080b0d6542626
Geraint <luffgd@gmail.com>
2014-02-18 10:26:16 +0000

    Add test for "dataPath" result with "required"

---
+++ test/tests/13 - error reporting/01 - required dataPath.js
@@ -0,0 +1,28 @@
+describe("Fill dataPath for \"required\" (GitHub Issue #103)", function () {
+	it("Blank for first-level properties", function () {
+		var schema = {
+			required: ['A']
+		};
+		var data = {};
+		
+		var result = tv4.validateMultiple(data, schema, false, true);
+		console.log(result);
+		assert.isFalse(result.valid, "Must not be valid");
+		assert.deepEqual(result.errors[0].dataPath, '');
+	});
+
+	it("Filled for second-level properties", function () {
+		var schema = {
+			properties: {
+				"foo": {
+					required: ["bar"]
+				}
+			}
+		};
+		var data = {"foo": {}};
+		
+		var result = tv4.validateMultiple(data, schema, false, true);
+		assert.isFalse(result.valid, "Must not be valid");
+		assert.deepEqual(result.errors[0].dataPath, '/foo');
+	});
+});
