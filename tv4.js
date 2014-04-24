01355944f2ce2294cbebe925b5df53c24a96d53b
Jake Rigby <jakewigby@gmail.com>
2014-04-24 13:23:19 +0100

    Added item validation for 'object' types to allow dictionary-like object validation
    var schema = {
    "type": "object",
    "items": {"$ref": "#item"},
    "definitions": {
    "item": {
    "id": "#item",
    "type": "object",
    "properties": {
    "prop_one": {"type": "string"}
    },
    "required": ["prop_one"]
    }
    }
    },
    
    obj = {
    "one" : {
    "prop_one": "1"
    },
    "two": {
    "prop_one": "2"
    }
    };
    
    console.log(tv4.validate(obj, schema));
    
    > true

--- tv4.js
+++ tv4.js
@@ -723,6 +723,7 @@ ValidatorContext.prototype.validateObject = function validateObject(data, schema
 		|| this.validateObjectRequiredProperties(data, schema, dataPointerPath)
 		|| this.validateObjectProperties(data, schema, dataPointerPath)
 		|| this.validateObjectDependencies(data, schema, dataPointerPath)
+		|| this.validateObjectItems(data, schema, dataPointerPath)
 		|| null;
 };
 
@@ -844,6 +845,20 @@ ValidatorContext.prototype.validateObjectDependencies = function validateObjectD
 				}
 			}
 		}
+	}
+	return null;
+};
+
+ValidatorContext.prototype.validateObjectItems = function validateArrayItems(data, schema, dataPointerPath) {
+	if (schema.items === undefined) {
+		return null;
+	}
+	var error, k;
+	for (var k in data) {
+		console.log('validating', data[k]);
+		if (error = this.validateAll(data[k], schema.items, [k], ["items"], dataPointerPath + "/" + k)) {
+			return error;
+		}		
 	}
 	return null;
 };
