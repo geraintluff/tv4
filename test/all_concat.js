66852546c66fbf1d4af38092dd6fea590fabcb99
Bartvds <bartvanderschoor@gmail.com>
2014-06-07 02:12:29 +0200

    enabled UMD for language file
    renamed index-require.html to index-amd.html
    - it tests AMD modules
    
    added added language moduel tests
    - commonJS test to existing cases
    - AMD test to index-amd.html
    - added global var test to index-globals.html

--- test/all_concat.js
+++ test/all_concat.js
@@ -2061,6 +2061,31 @@ describe("Register custom keyword", function () {
 	});
 });
 
+describe("Load language file", function () {
+	if (!process || !require) {
+		it.skip("commonjs language", function () {
+			// dummy
+		});
+	}
+	else {
+		it("commonjs language: de", function () {
+			var lang = require('../lang/de');
+
+			tv4.addLanguage('de', lang);
+			tv4.language('de');
+
+			var schema = {
+				properties: {
+					intKey: {"type": "integer"}
+				}
+			};
+			var res = tv4.validateResult({intKey: 'bad'}, schema);
+			assert.isFalse(res.valid);
+			assert.equal(res.error.message, 'Ungültiger Typ: string (erwartet wurde: integer)');
+		});
+	}
+});
+
 describe("Issue 108", function () {
 
 	it("Normalise schemas even inside $ref", function () {
